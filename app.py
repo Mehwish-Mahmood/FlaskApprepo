809b4f8704cd52c5bd30ba3bd1083d7740089d62
Mehwish-Mahmood <mehwishmahmood1205@gmail.com>
2026-01-18 23:56:40 +0530

    Fix port argument in app.run() call

--- app.py
+++ app.py
@@ -20,4 +20,4 @@ def home():
      study_hours=study_hours,
      attendance=attendance)
   if __name__== "__main__":
-    app.run(host="0.0.0.0.", port7860)
+    app.run(host="0.0.0.0.", port=7860)
